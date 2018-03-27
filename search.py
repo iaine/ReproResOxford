6e234c147b38f527b1b497face46b327fec68f51
Iain Emsley <iain.emsley@kellogg.ox.ac.uk>
2018-03-27 14:02:16 +0100

    Fix to sroting by year

--- search.py
+++ search.py
@@ -5,10 +5,11 @@
 import pandas as pd
 from ggplot import *
 
-g=pd.read_csv("/Users/iain.emsley/git/Texts/TCP.csv")
+g=pd.read_csv("data/TCP.csv")
 f=g.loc[g['Author'].str.contains("Luther",na=False)]
 freq = f['Date'].value_counts()
 n = pd.DataFrame({'Publication':freq.index, 'Freq':freq.values})
+n = n.sort_values(by=["Publication"])
 
 plt = ggplot(aes(x="Publication",y="Freq"), data = n) + geom_point() \
     + ggtitle("Works attributed to Luther") + xlab("Publication Date") \
