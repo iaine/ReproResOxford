30afb9761d4c6c299ead68323c617a41104e9c70
Iain Emsley <I.Emsley@sussex.ac.uk>
2018-07-05 21:33:44 +0100

    Update variable names

--- search.py
+++ search.py
@@ -1,17 +1,19 @@
 '''
    Script to search EEBO catalogue
 '''
+#uncomment if using Jupyter
+#%matplotlib inline
 
 import pandas as pd
 from ggplot import *
 
-g=pd.read_csv("data/TCP.csv")
-f=g.loc[g['Author'].str.contains("Luther",na=False)]
-freq = f['Date'].value_counts()
-n = pd.DataFrame({'Publication':freq.index, 'Freq':freq.values})
-n = n.sort_values(by=["Publication"])
+authors=pd.read_csv("data/TCP.csv")
+luther=authors.loc[authors['Author'].str.contains("Luther",na=False)]
+freq = luther['Date'].value_counts()
+freq_data = pd.DataFrame({'Publication':freq.index, 'Freq':freq.values})
+freq_data = freq_data.sort_values(by=["Publication"])
 
-plt = ggplot(aes(x="Publication",y="Freq"), data = n) + geom_point() \
+plt = ggplot(aes(x="Publication",y="Freq", weight="Publication"), data = freq_data) + geom_point() \
     + ggtitle("Works attributed to Luther") + xlab("Publication Date") \
     + ylab("Number of publications")
 
